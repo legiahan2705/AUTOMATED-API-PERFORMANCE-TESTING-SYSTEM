2e308c6667ce9cb96fe5413f7c25accce35cad2c
legiahan2705 <legiahancv@gmail.com>
2025-09-07 17:14:03 +0700

    update

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { AppSidebar } from "@/components/app-sidebar";
 import { SectionCards } from "@/components/section-cards";
--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -1,6 +1,4 @@
-"use client"
-
-
+"use client";
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
